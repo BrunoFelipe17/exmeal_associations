d1343dcb75c4dd66027a08ac3ec7aecaa827e7fd
Bruno Felipe <bruno.felipe7170@gmail.com>
2021-08-16 12:30:56 -0300

    Created module to insert User on DB

--- lib/exmeal/user/create.ex
+++ lib/exmeal/user/create.ex
@@ -1,6 +1,17 @@
 defmodule Exmeal.Users.Create do
+  alias Exmeal.{Error, Repo, User}
 
-  def call() do
+  def call(params) do
     # TO DO
+    params
+    |> User.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
   end
 end
