38bbf2179fd350751765a8f9b81c1b3357ac9f55
Bruno Felipe <bruno.felipe7170@gmail.com>
2021-08-16 12:04:06 -0300

    Added a reference to user table on meals table

--- priv/repo/migrations/20210518195336_create_meals_table.exs
+++ priv/repo/migrations/20210518195336_create_meals_table.exs
@@ -6,6 +6,7 @@ defmodule Exmeal.Repo.Migrations.CreateMealsTable do
       add :calories, :integer
       add :date, :date
       add :description, :string
+      add :user_id, references(:users, type: :binary_id)
 
       timestamps()
     end
